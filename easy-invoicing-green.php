4d814a08eebd045b381a026568b3a7c36b274e42
felipeeco <contacto@felipemoreno.co>
2023-05-27 12:45:08 -0500

    sincronia con el servidor

--- easy-invoicing-green.php
+++ easy-invoicing-green.php
@@ -1,46 +1,34 @@
 <!DOCTYPE html>
 <?php
-$title = 'Welcome';
-$loggin = false; /*TEST: SHOW ELEMENTS WHEN USER LOGGED IN OR NOT*/
-$body_class = 'easy-invoicing-green';
+$title = 'Faktuer uten a starte eget firma';
+$loggin = true; /*TEST: SHOW ELEMENTS WHEN USER LOGGED IN OR NOT*/
+$body_class = 'easy-invoicing-green page no-back';
 $load_ratingModal = false;
 ?>
 <?php include ('template-parts/head.php')?>
-        <div class="background-box">
-            <div class="container">
-			    <div class="row">
-				    <div class="col-xl-8 box-left">
-                        <img src="./images/easy-invoicing/logo.svg" alt="logo" class='easy-invoicing-logo'>
-						<h1>Faktuer uten a starte eget firma</h1>
-						<p>
-                            <strong>MinSmajobb</strong> ogg <strong>EasyFreelance</strong> har
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategori.
-                        </p>
-                        <a href="javascript:;" class="btn btn-secondary">
-                            <i class="fa-solid fa-check"></i>
-                            Register
-                        </a>
-                        <a href="javascript:;" class="btn btn-transparent">
-                            <i class="fa-solid fa-circle-info"></i>
-                            More Information
-                        </a>
-					</div>
-                    <div class="col-xl-4 box-right">
-                        <img src="./images/easy-invoicing/example-image.png" alt="people">
-                    </div>
-                    <div class="col-xl-12 box-video mx-auto text-center">
-                        <h3>Se hvordan det fungerer?</h3>
-                        <iframe width="617" height="345" src="https://www.youtube.com/embed/Bk8Ll0qaA48?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    </div>
+	</div><!--END OF DIV CLASS LANDING-->
+<div class="gradient-bg">
+	<div class="container">
+		<div class="row py-5">
+			<div class="col-md-8">
+				<img src="./images/easy-invoicing/logo.svg" alt="logo" class='easy-invoicing-logo'>
+				<h1>Faktuer uten a starte eget firma</h1>
+				<p class="lead"><strong>MinSmajobb</strong> ogg <strong>EasyFreelance</strong> har inngått et samarbeid for å gjøre det enkelt for deg som ikke har din egen virksomhet å fakturere oppdragsgivere. Det tar bare noen få minutter å registrere en konto og sende din første faktura. Med EasyFreelance kan du være trygg på at skatter og avgifter blir håndtert, og at du vil motta din utbetaling som lønn på din konto. Det beste av alt? Alt skjer automatisk, slik at du kan bruke mer tid på å fokusere på dine oppdrag.</p>
+				<div class="mt-4">
+					<a href="javascript:;" class="btn btn-secondary btn-lg"><i class="fa-solid fa-check"></i>Register</a>
+					<a href="javascript:;" class="btn btn-outline-light btn-lg"><i class="fa-solid fa-circle-info"></i>More Information </a>
 				</div>
 			</div>
-        </div>
-	</div><!--END OF DIV CLASS LANDING-->
-
+			<div class="col-md-4">
+				<img src="./images/easy-invoicing/example-image.svg" alt="people">
+			</div>
+		</div>
+		<div class="row py-5">
+			<div class="col-xl-12 box-video mx-auto text-center">
+				<h1 class="mb-4">Se hvordan det fungerer?</h1>
+				<iframe width="617" height="345" src="https://www.youtube.com/embed/Bk8Ll0qaA48?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+</div>
 <?php include ('template-parts/footer.php')?>
--- pwa/easy-invoicing.php
+++ pwa/easy-invoicing.php
@@ -6,7 +6,7 @@ UPDATES April 2022
 -->
 <!doctype html>
 <?php 
-$title = "Dashboard"; /*PAGE TITLE*/
+$title = "Easy Invoicing"; /*PAGE TITLE*/
 $loggin = true; /*TEST: SHOW ELEMENTS WHEN USER LOGGED IN OR NOT*/
 $hasnavbar = false; /*LOAD THE BOTTOM NAVIGATION BAR IF NEEDED*/
 $body_class = "gradient-bg";
@@ -20,104 +20,26 @@ $os_type = 'ios';
 ?>
 <?php include ('template-parts/head.php')?>
 <!--PAGE CONTENT-->
-	<!-- rating modals -->
-	<?php if ($os_type == 'ios'){
-		include ('template-parts/modals/ios-modal.php'); //iOS modal path
-	} else if ($os_type == 'android'){
-		include ('template-parts/modals/android-modal.php'); //Andoird Modal path
-	}
-	?>
-<!-- MODAL CONTENT SAMPLE STARTS HERE -->
-<!-- Modal -->
-<div class="modal fade" id="stept-1" tabindex="-1" aria-labelledby="stept-1Label" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="stept-1Label"><i class="fa-solid fa-tag"></i> BYGMAKKER</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true"><i class="far fa-times-circle"></i></span>
-        </button>
-      </div>
-      <div class="modal-body text-center">
-        <img src="../images/pwa/sample/byggmakker-logo-2.png" alt="" class="brand-image m-auto w-50 d-block">
-		  <h5>30% on all ordinary products</h5>
-		  <p>Scan the barcode when you pay for your products</p>
-		  <img src="../images/pwa/sample/barcode-sample.png" alt="" class="bar-code w-80 d-block m-auto">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-light" type="button" class="btn btn-primary" data-toggle="modal" data-target="#stept-2" data-dismiss="modal">Close</button>
-        <!--<button type="button" class="btn btn-light" data-dismiss="modal">Close</button>-->
-      </div>
-    </div>
-  </div>
+<div class="d-flex fixed-bottom dark-bg p-3 g-10">
+	<a href="javascript:;" class="btn btn-outline-light btn-lg"><i class="fa-solid fa-circle-info"></i>Information </a>
+	<a href="javascript:;" class="btn btn-primary btn-lg"><i class="fa-solid fa-check"></i>Register</a>
 </div>
-
-<div class="modal fade" id="stept-2" tabindex="-1" aria-labelledby="stept-2Label" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h5 class="modal-title w-100" id="stept-2Label"><i class="fa-solid fa-check"></i> Did you do a purchase?</h5>
-      </div>
-      <div class="modal-body text-center">
-		<div class="card grey-bg mb-4">
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item d-flex align-items-center"><label for="Your ID" class="m-0">Your ID:</label><h5 class="green flex-grow-1 text-right">#66531</h5></li>
-				<li class="list-group-item d-flex align-items-center"><label for="Date of purchase:" class="m-0">Date of purchase:</label><h5 class="flex-grow-1 text-right">10.12.2022</h5></li>
-				<li class="list-group-item d-flex align-items-center"><label for="Aproxinate time:" class="m-0">Aproxinate time:</label><h5 class="flex-grow-1 text-right">18:30</h5></li>
-			</ul>
-		</div>
-		<p>You can enter the aprox amount you paid.</p>
-		<p>This helps us maintain this deal or make a even better deal for our userrs in the future.</p>
-		<div class="input-group input-group-lg mb-3">
-			<input type="text" class="form-control" placeholder="0.00" aria-label="Amount Paid" aria-describedby="basic-addon2">
-			<div class="input-group-append">
-				<span class="input-group-text" id="basic-addon2">Kr.</span>
-			</div>
-		</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-light" data-dismiss="modal">I didn't Buy</button>
-        <button type="button" class="btn btn-primary" data-dismiss="modal" >Finish</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- /.modal -->
-<!-- END MODAL CONTENT SAMPLE -->
-	<!-- end rating modals -->
-	<div class="landing nopic pwa d-flex easy-invoicing-pwa" style="height: calc(100vh - 290px); ">
+	<div class="easy-invoicing-pwa">
 		<div class="container">
 			<div class="row">
-				<div class='col-12 text-center main-image'>
-					<img src="../images/easy-invoicing/example-image.png" alt="slide">
+				<div class="col-md-4">
+					<img src="../images/easy-invoicing/example-image.svg" alt="people" width="100%">
 				</div>
-				<div class='col-12 text-left description-box'>
+				<div class="col-md-8">
 					<img src="../images/easy-invoicing/logo.svg" alt="logo" class='easy-invoicing-logo'>
 					<h1>Faktuer uten a starte eget firma</h1>
-					<p>
-                        <strong>MinSmajobb</strong> ogg <strong>EasyFreelance</strong> har
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategoriÅ legge ut et oppdrag er enkelt og gratis. 
-                            Start ved å velge en kategori.
-                    </p>
+					<p class="lead"><strong>MinSmajobb</strong> ogg <strong>EasyFreelance</strong> har inngått et samarbeid for å gjøre det enkelt for deg som ikke har din egen virksomhet å fakturere oppdragsgivere. Det tar bare noen få minutter å registrere en konto og sende din første faktura. Med EasyFreelance kan du være trygg på at skatter og avgifter blir håndtert, og at du vil motta din utbetaling som lønn på din konto. Det beste av alt? Alt skjer automatisk, slik at du kan bruke mer tid på å fokusere på dine oppdrag.</p>
 				</div>
-				<div class='col-12 text-left video-box'>
-					<h3>Se hvordan det fungerer?</h3>
-                    <iframe width="100%" height="196" src="https://www.youtube.com/embed/Bk8Ll0qaA48?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-				</div>
-				<div class='col-12 text-left call-to-action'>
-					<a href="javascript:;" class="btn btn-transparent">
-                        <i class="fa-solid fa-circle-info"></i>
-                        Information
-                    </a>
-					<a href="javascript:;" class="btn btn-primary">
-                        <i class="fa-solid fa-check"></i>
-                    	Register
-                    </a>
+			</div>
+			<div class="row py-5">
+				<div class="col-12 box-video mx-auto text-center mb-5">
+					<h1 class="mb-4">Se hvordan det fungerer?</h1>
+					<iframe width="100%" height="345" src="https://www.youtube.com/embed/Bk8Ll0qaA48?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
